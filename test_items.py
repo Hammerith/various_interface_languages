334fe0a10aece4b2ec82291998dcdd181a360430
Hammerith <hammerith@yandex.ru>
2020-06-30 17:53:35 +0300

    added assert

--- test_items.py
+++ test_items.py
@@ -1,6 +1,11 @@
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_add_basket(browser):
+def test_guest_should_see_add_basket_button(browser):
     browser.get(link)
-    browser.find_element_by_css_selector(".btn-add-to-basket")
+    button_add = len(browser.find_elements_by_css_selector(".btn-add-to-basket"))
+    assert button_add > 0, 'button not found'
+
+
+
+
