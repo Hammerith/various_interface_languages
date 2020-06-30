55882b015f1ed3d5104d6cbcf389fa4971d437eb
Hammerith <hammerith@yandex.ru>
2020-06-30 17:57:45 +0300

    added #time.sleep(30)

--- test_items.py
+++ test_items.py
@@ -1,8 +1,11 @@
+import time
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_guest_should_see_add_basket_button(browser):
     browser.get(link)
+    # time.sleep(30)
     button_add = len(browser.find_elements_by_css_selector(".btn-add-to-basket"))
     assert button_add > 0, 'button not found'
 
